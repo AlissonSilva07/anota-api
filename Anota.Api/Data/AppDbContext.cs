effb5f66100af9f542fc226f59ccbb88d91b328f
Alisson Rego <alissondroid07@gmail.com>
2026-05-22 10:07:51 -0300

    chore: create initial migration of the users table

---
+++ Anota.Api/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Anota.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anota.Api.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
