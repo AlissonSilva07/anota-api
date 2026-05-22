1c9ee55d28bb110127b719a740b771a8ef8794a4
Alisson Rego <alissondroid07@gmail.com>
2026-05-22 09:26:04 -0300

    feat: add register route and scalar

--- Anota.Api/Controllers/AuthController.cs
+++ Anota.Api/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Anota.Api.Entities;
+using Anota.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Anota.Api.Controllers
@@ -7,5 +10,18 @@ namespace Anota.Api.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                 .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
     }
 }
