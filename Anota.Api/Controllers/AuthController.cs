a15974c46093b85024fc513cd5dc84ecc8f55119
Alisson Rego <alissondroid07@gmail.com>
2026-05-22 09:51:52 -0300

    feat: add create token function

--- Anota.Api/Controllers/AuthController.cs
+++ Anota.Api/Controllers/AuthController.cs
@@ -3,12 +3,16 @@ using Anota.Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Anota.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IConfiguration config) : ControllerBase
     {
         public static User user = new();
 
@@ -38,9 +42,31 @@ namespace Anota.Api.Controllers
                 return BadRequest("Wrong password.");
             }
 
-            string token = "success";
+            string token = CreateToken(user);
 
             return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: config.GetValue<string>("AppSettings:Issuer"),
+                audience: config.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
         }
     }
 }
